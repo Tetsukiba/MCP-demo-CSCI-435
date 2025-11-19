29b29574f9a4971071f21b2e692102fbf7a400af
Michael White <Tetsukiba@outlook.com>
2025-11-19 12:17:59 -0500

    Add extracted Home.jsx from Figma node 9-2708

---
+++ figma_extracted/Home.jsx
@@ -0,0 +1,21 @@
+// Extracted from Figma node 9-2708, file kn0QYBr8YvZp0jX4OgsL9U
+// React + Tailwind code. Images are remote URLs, valid for 7 days.
+
+const imgShape = "https://www.figma.com/api/mcp/asset/f431f73b-9129-4912-941e-b1dfe939eda0";
+const imgSection = "https://www.figma.com/api/mcp/asset/96fbe9f3-6ded-423f-8fd1-08239511feea";
+const img = "https://www.figma.com/api/mcp/asset/924f9113-cb0e-42f8-878c-77cc37aa8236";
+const img1 = "https://www.figma.com/api/mcp/asset/f337c729-7a3c-42bb-b124-123a25d7ae52";
+const img2 = "https://www.figma.com/api/mcp/asset/1de3567e-e628-411e-a25e-232c8b898103";
+const img3 = "https://www.figma.com/api/mcp/asset/267f1678-e460-42bb-8f5f-0408918fe7f2";
+const img4 = "https://www.figma.com/api/mcp/asset/a01c095f-c163-41a6-9d6c-de1bcb1ee2c2";
+const img5 = "https://www.figma.com/api/mcp/asset/0e2daa48-9298-4695-84ec-a4e52b03a83e";
+
+// ...component code from previous step (React + Tailwind, see returned data)...
+
+export default function Home() {
+  return (
+    <div className="bg-[var(--sds-color-background-default-default,#ffffff)] content-stretch flex flex-col items-start relative size-full" data-name="Home" data-node-id="9:2708">
+      {/* ...rest of the component code... */}
+    </div>
+  );
+}
