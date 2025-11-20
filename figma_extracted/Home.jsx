9797f619666ab71141ca496d4539d547a3e3a495
Michael White <Tetsukiba@outlook.com>
2025-11-19 22:00:38 -0500

    feat(figma): add extracted Home.jsx from Figma

---
+++ figma_extracted/Home.jsx
@@ -0,0 +1,12 @@
+const imgShape = "https://www.figma.com/api/mcp/asset/5da1838d-28af-4e6e-85a0-b4fb0cca1361";
+const imgSection = "https://www.figma.com/api/mcp/asset/216b0c6d-07d3-4c32-82ba-8797f0836020";
+import { img, img1, img2, img3, img4, img5 } from "./svg-hhylr";
+
+export default function Home() {
+  return (
+    <div className="bg-[var(--sds-color-background-default-default,#ffffff)] content-stretch flex flex-col items-start relative size-full" data-name="Home" data-node-id="9:2708">
+      <div className="bg-[var(--sds-color-background-default-default,#ffffff)] border-[var(--sds-color-border-default-default,#d9d9d9)] border-b-[var(--sds-size-stroke-border,1px)] border-l-0 border-r-0 border-solid border-t-0 relative shrink-0 w-full" data-name="Header" data-node-id="9:2709">
+        ...
+    </div>
+  );
+}
